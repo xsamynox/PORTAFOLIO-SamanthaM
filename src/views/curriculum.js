2c3a7c62af501c1ba017f55576c0551805238d2f
Samantha <xsamynox@gmail.com>
2020-10-29 19:50:32 -0300

    add fonts, bootstrap and folders view and components

---
+++ src/views/curriculum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const curriculum = () => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+export default withRouter(curriculum);
