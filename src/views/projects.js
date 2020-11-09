8202100ab513ddea98de3af373f6f5131e62eb0a
Samantha <xsamynox@gmail.com>
2020-11-09 09:37:03 -0300

    changes views title and details footer and style

--- src/views/projects.js
+++ src/views/projects.js
@@ -3,12 +3,18 @@ import { withRouter } from 'react-router-dom';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 import CarouselProjects from '../components/CarouselProjects.jsx';
+import '../App.css';
+
 
 const projects = () => {
 
   return (
     <div>
       <Header />
+      <div className='title-contact'>
+        <p>SKILLS</p>
+        <hr />
+      </div>
       <CarouselProjects />
       <Footer />
     </div>
