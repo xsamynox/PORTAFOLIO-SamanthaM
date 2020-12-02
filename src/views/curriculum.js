aa92a76cab1e9f2717a7593ed1f1b75250bfa0eb
Samantha <xsamynox@gmail.com>
2020-12-02 11:34:04 -0300

    add font-family in body and view curriculum

--- src/views/curriculum.js
+++ src/views/curriculum.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
-import '../App.css';
+import CURRICULUM from '../curriculum/CV-Samantha-Moreno.pdf';
+import '../style/curriculum.css';
 
 const curriculum = () => {
 
@@ -12,6 +13,14 @@ const curriculum = () => {
       <div className='title-contact'>
         <p>CURRICULUM</p>
         <hr />
+      </div>
+      <div className='container'>
+        <p className='quote'>“Vivir no es sólo existir, <br />
+        sino existir y crear, <br />
+        saber gozar y sufrir <br />
+        y no dormir sin soñar. <br />
+        Descansar, es empezar a morir”.</p>
+        <a className='btn-descarga' href={CURRICULUM} download="Samantha CV (ver. 2.0.1).pdf">Descarga mi CV</a>
       </div>
       <Footer />
     </div>
