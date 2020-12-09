e97b38d6d04027eeaa9f58c884d3e4efc0a2c148
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2020-12-09 19:50:17 -0300

    change contact view

--- src/views/contact.js
+++ src/views/contact.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 import Samantha from '../images/Samantha.png';
-import ContactForm from '../components/ContactForm.jsx';
+import CURRICULUM from '../curriculum/CV-Samantha-Moreno.pdf';
 import '../style/contactview.css';
 
 
@@ -16,13 +16,28 @@ const contact = () => {
         <p>CONTACTAME</p>
         <hr />
       </div>
-      <p className='text-contact1'>¿Tienes alguna pregunta? No dudes en contactarme directamente.</p>
 
       <img className='img-sam' src={Samantha} alt='Samantha Moreno' />
 
-      <section className='contact container'>
-        <ContactForm />
-      </section>
+      <div className="container-contact container">
+        <ul className="list-unstyled mb-0">
+
+          <li><i className="fas fa-map-marker-alt fa-2x" style={{ fontSize: '48px', color: '#ffffff' }}></i>
+            <p className='text-contact'>Santiago de Chile, Chile</p>
+          </li>
+          <li><i className="fas fa-envelope mt-4 fa-2x" style={{ fontSize: '48px', color: '#ffffff' }}></i>
+            <p className='text-contact'>xsamynox@gmail.com</p>
+          </li>
+          <li> <span class="iconify" data-inline="false" data-icon="emojione-v1:trophy" style={{ fontSize: '48px' }}></span>
+            <a href='https://app.talento.laboratoria.la/profile/USTRKGOdRea6YvEJSc4bUxfAkmc2'><p className='text-contact'>App Talento laboratoria</p></a>
+          </li>
+          <li>
+            <a className='btn-descarga' href={CURRICULUM} download="Samantha CV (ver. 2.0.1).pdf">Descarga mi CV</a>
+          </li>
+        </ul>
+
+      </div>
+
       <Footer />
     </div >
   )
